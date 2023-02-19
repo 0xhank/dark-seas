41e96796b507a39cde4941743a37b98361f87caf
0xhank <zeroxhank@gmail.com>
2023-02-19 14:17:35 -0500

    fix: get it working in parcel

--- packages/contracts/src/test/LibDeploy.sol
+++ packages/contracts/src/test/LibDeploy.sol
@@ -186,7 +186,11 @@ library LibDeploy {
     deploySystems(address(result.world), true);
   }
 
-  function authorizeWriter(IUint256Component components, uint256 componentId, address writer) internal {
+  function authorizeWriter(
+    IUint256Component components,
+    uint256 componentId,
+    address writer
+  ) internal {
     IComponent(getAddressById(components, componentId)).authorizeWriter(writer);
   }
 
