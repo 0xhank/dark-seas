3b4c8da0280097fd2544404449cd18eafc109da4
0xhank <zeroxhank@gmail.com>
2023-03-08 14:23:50 -0500

    chore: delete unused files

--- packages/contracts/src/scripts/constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.componentsDir = exports.systemsDir = void 0;
-exports.systemsDir = "systems";
-exports.componentsDir = "components";
--- packages/contracts/src/scripts/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateAndDeploy = void 0;
-var deploy_1 = require("./deploy");
-Object.defineProperty(exports, "generateAndDeploy", { enumerable: true, get: function () { return deploy_1.generateAndDeploy; } });
